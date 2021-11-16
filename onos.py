1e16058ad660a56756dd2f06643fb3c1707682bc
Alisson <alissonp.medeiros@gmail.com>
2021-11-16 10:55:23 +0100

    adding munch dict, dataclasses and built in functions to onos

--- onos.py
+++ onos.py
@@ -1,94 +1,101 @@
 #!/usr/bin/python3
-
 import requests
 from requests.exceptions import HTTPError
+from dataclasses import dataclass
+from munch import Munch, DefaultMunch
 import pprint
 
 
-
-
+@dataclass(frozen=True)
 class OnosController:
-    def __init__(self):
-        self.server_IP = '130.92.70.174'
-        self.OF_port = '8181'
-        self.user = 'onos'
-        self.password = 'rocks' 
+    """ SDN controller representation for ONOS """
+    
+    server_IP: str = '130.92.70.174'
+    OF_port: str = '8181'
+    user: str = 'onos'
+    password: str = 'rocks' 
 
 
-    def request(self, resource: str):
+    def request(self, resource: str) -> dict:
+        """ provides an http request to onos server API """
+        
         URL = 'http://' + self.server_IP + ':' + self.OF_port + '/onos/v1/' + resource 
         try: 
             response = requests.get(URL, auth=(self.user, self.password), verify=False)
-            return response.json()
+            result = DefaultMunch.fromDict(response.json())
+            return result
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
 
 
-    def get_hosts(self):
+    def get_hosts(self) -> dict:
+        """ get the list of mininet hosts, including stations and hosts """
+        
         resource = 'hosts'
         hosts = self.request(resource)
         return hosts
 
 
-    def get_host(self, host_IP: str):
+    def get_host(self, host_IP: str) -> list:
+        """ get a specific host info based on its IP""" 
+        """
+        usage -> host['key']
+        during this loop, the following keys are available for each host:
+        id, mac, vlan, innerVlan, outerTpid, configured, suspended,
+        ipAddresses[0], locations[0]['elementId'] 
+        """
+        
         hosts = self.get_hosts()
-        for host in hosts['hosts']:
-            '''
-            usage -> host['key']
-            during this loop, the following keys are available for each host:
-            id, mac, vlan, innerVlan, outerTpid, configured, suspended,
-            ipAddresses[0], locations[0]['elementId'] 
-            '''
-            if host['ipAddresses'][0] == host_IP:
-                print(host)
-                return host
+        return [host for host in hosts.hosts if host.ipAddresses[0] == host_IP]
+        
 
-        print('Host with ip ' + host_IP + ' not found')
-        return None
 
-    def get_devices(self):
-        # this function returns a list of devices, where each one of them is a dict object
+
+    def get_devices(self) -> list:
+        """ returns a list of devices, which includes mininet switches and access points """
+        
         resource = 'devices'
         devices = self.request(resource)
         devices_list = []
 
-        for device in devices['devices']:
-            '''
+        for device in devices.devices:
+            """
             usage -> device['key']
             during this loop, the following keys are available for each device:
             id, chassisId, annotations['datapathDescription']. 
             annotations['datapathDescription'] contains the device name from mininet  
-            '''
-            device_id = device['id']
-            device_name = device['annotations']['datapathDescription']
-            new_device = {'id': device_id, 'name': device_name}
+            """
+
+            device_id = device.id
+            device_name = device.annotations.datapathDescription
+            new_device = Munch ({'id': device_id, 'name': device_name})
             devices_list.append(new_device)
         
         return devices_list
 
 
-    def get_device(self, device_id: str):
+    def get_device(self, device_id: str) -> list:
+        """ get a specific device info based on its ID""" 
+
         devices = self.get_devices()
-        for device in devices:
-            if device['id'] == device_id:
-                print(device)
-                return device
-        return None    
+        print(type(devices))
+        return [device for device in devices if device.id == device_id]
+        
 
+    def get_links(self) -> list:
+        """ returns a list of links, including the source and destination devices and the used ports """
 
-    def get_links(self):
-        # this function returns a list of links, where each one of them is a dict object
         resource = 'links'
         links = self.request(resource)
         links_list = []
-
-        for link in links['links']:
-            '''
+        for link in links.links:
+            """
             usage -> link['key']
             during this loop, the following keys are available for each link:
             src['port'], src['device'], dst['port'], dst['device'], state, type. 
             Furthermore, scr = source and dst = destination
-            '''
+            """
+
             scr_port = link['src']['port']
             scr_device = link['src']['device']
             dst_port = link['dst']['port']
@@ -96,36 +103,40 @@ class OnosController:
 
             new_link = {'scr': {'port': scr_port, 'device': scr_device}, 
                         'dst': {'port': dst_port, 'device': dst_device}}
+            
+            new_link_dict = DefaultMunch.fromDict(new_link)
 
-            links_list.append(new_link)
+            links_list.append(new_link_dict)
 
         return links_list
 
 
-    def get_link(self, node_id: str):
+    def get_link(self, device_id: str) -> list:
+        """ get the associated (destination) links to a specific device""" 
+
         links = self.get_links()
-        device_links = []
-        for link in links:
-            if link['scr']['device'] == node_id:
-                device_links.append(link)
-        
-        return device_links
+        return [link for link in links if link.scr.device == device_id]
 
 
-    def get_flows(self):
+    def get_flows(self) -> dict:
+        """ returns the list of flows deployed on the devices"""
+
         resource = 'flows'
         flows = self.request(resource)
         return flows
 
 
-    def get_intents(self):
+    def get_intents(self) -> dict:
+        """ returns the list of intents, which are used to manage the network resources (bandwidth, flows)"""
+
         resource = 'intents'
         intents = self.request(resource)
         return intents
 
 
-    def get_topology(self):
-        # this function builds the topology of the network, containing devices and links
+    def get_topology(self) -> list:
+        """" builds the topology of the network, containing devices and links """
+
         devices = self.get_devices()
         topology = []
         for device in devices: 
@@ -140,7 +151,16 @@ class OnosController:
 
 
 
+'''
+
 if __name__ == '__main__':
     controller = OnosController()
-    x = controller.get_topology()
+    #x = controller.get_device('of:1000000000000004')
+    x = controller.get_link('of:1000000000000002')
+    #x = controller.get_links()
+    print("\n")
     pprint.pprint(x)
+    for link in x:
+        print(link.dst.device)
+    
+'''
